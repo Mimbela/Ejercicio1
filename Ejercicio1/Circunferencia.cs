4f845c07623f7eab4e8a6f8587d400fe87f43260
senin8 <malizziaaries@gmail.com>
2018-05-21 23:05:02 +0200

    Add project files.

---
+++ Ejercicio1/Circunferencia.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class Circunferencia
+    {
+        private double radio;
+
+        public double Area()
+        {
+            double area = Math.PI * radio * radio;
+
+            return area;
+        }
+        public double Perimetro()
+        {
+            double perimetro = 2 * Math.PI * radio;
+            return perimetro;
+        }
+
+        public void ModificarRadio(double newRadio)
+        {
+            radio = newRadio;
+        }
+
+        public double ConsultarRadio()
+        {
+            return radio;
+        }
+    }
+}
+
+//EJERCICIO 1
+//Declarar una clase llamada Circunferencia que tenga como atributo privado el radio de tipo real y los siguientes métodos:
+//a) Calcular el área.
+//b) Calcular el perímetro.
+//c) Método para modificar el radio
+//d) Método para consultar el valor del radio
+//Declara dos objetos: Rueda y Moneda y realiza las siguientes operaciones:
+//a) Inicializa el radio de la Rueda con el valor de 10.2
+//b) Inicializa el radio de la Moneda con el valor de 1.4
+//c) Imprime el área de la Rueda.
+//d) Imprime el área de la Moneda.
+//e) Imprime el perímetro de la Rueda.
+//f) Imprime el perímetro de la Moneda.
+
+    
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circunferencia Rueda = new Circunferencia();
+            Rueda.ModificarRadio(10.2);
+
+            Circunferencia Moneda = new Circunferencia();
+            Moneda.ModificarRadio(1.4);
+
+            Console.WriteLine("AREA RUEDA: {0}", Rueda.Area());
+            Console.WriteLine("AREA MONEDA: {0}", Moneda.Area());
+            Console.WriteLine("PERIMETRO RUEDA: {0}", Rueda.Perimetro());
+            Console.WriteLine("PERIMETRO MONEDA: {0}", Moneda.Perimetro());
+
+            Console.Read();
+        }
+    }
+}
